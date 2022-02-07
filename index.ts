441a72c04cfc61dfead5cad37202a1da87a44499
Jan Tuomi <jans.tuomi@gmail.com>
2022-02-07 14:11:24 +0200

    Add Example API types

--- index.ts
+++ index.ts
@@ -1,12 +1,32 @@
-export namespace RunnerApiSubmitRequest {
-  export const path = "/submit"
-  export interface Body {
-    source: string
+export namespace RunnerApi {
+  export namespace SubmitRequest {
+    export const path = "/submit"
+    export interface Body {
+      source: string
+    }
+  }
+
+  export namespace SubmitResponse {
+    export interface Body {
+      result: string
+    }
   }
 }
 
-export namespace RunnerApiSubmitResponse {
-  export interface Body {
-    result: string
+export namespace ExampleApi {
+  export interface Example {
+    id: string
+    title: string
+    content: string
+  }
+
+  export namespace ListRequest {
+    export const path = "/"
+  }
+
+  export namespace ListResponse {
+    export interface Body {
+      examples: Example[]
+    }
   }
 }
